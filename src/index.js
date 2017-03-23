c23132ef51db1080dea9a19d5614e586abaf460b
Rein Van Oyen <reinvanoyen@gmail.com>
2017-03-23 09:49:47 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const Form = require( './form' ),
-	TextField = require( './component/TextField' )
+const $ = require( 'jquery' ),
+	Form = require( './form' ),
+	TextField = require( './component/textfield' ),
+	Select = require( './component/select' )
 ;
 
 let form =  new Form( 1, {
@@ -12,13 +14,19 @@ form.addField( new TextField( 1 ) );
 form.addField( new TextField( 2 ) );
 form.addField( new TextField( 3 ) );
 
-form.addField( new Select( 4, [ 'None', 'Vegetarian', 'Etc' ], {
-	v8nRequired: true
-} ) );
+form.addField( new Select( 4, [ 'None', 'Vegetarian', 'Etc' ] ) );
+form.addField( new Select( 5, [ 'Pick me', 'Or me', 'Nooo, pick me!' ] ) );
 
 form.build().appendTo('body');
 
 form.on( 'submit', e => {
 
-	alert( JSON.stringify( e.data ) )
+	$( '<textarea>' )
+		.css( {
+			height: 300,
+			width: 300
+		} )
+		.appendTo('body')
+		.text( JSON.stringify( e.data, null, '\t' ) )
+	;
 } );
