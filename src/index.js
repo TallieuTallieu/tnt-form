d191851d88a15f0b23acc45ed3a0125ef7e310aa
Rein Van Oyen <reinvanoyen@gmail.com>
2017-03-23 09:14:23 +0100

    Select field design

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const Form = require( './form' ),
 	TextField = require( './component/TextField' )
 ;
 
-let form =  new Form( 1 );
+let form =  new Form( 1, {
+	submitButtonText: 'Give me the values!'
+} );
 
 form.addField( new TextField( 1 ) );
 form.addField( new TextField( 2 ) );
 form.addField( new TextField( 3 ) );
+
+form.addField( new Select( 4, [ 'None', 'Vegetarian', 'Etc' ], {
+	v8nRequired: true
+} ) );
 
 form.build().appendTo('body');
 
